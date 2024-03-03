a20428fc318ee4f8e800b3d27713967c266d9e87
Mithun-18 <mithunpoojary180@gmail.com>
2024-03-03 20:24:28 +0530

    login controller added

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,6 +1,18 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { connectionPool } from "../db/index.js";
 
 const loginUserController = asyncHandler(async (req, res) => {
+  const { password, userName } = req.body;
+  try {
+    const connection = await connectionPool.getConnection();
+    const sql = "select * from tbl_users";
+    const [results] = await connection.query(sql);
+    console.log(results);
+    connection.release();
+  } catch (error) {
+    console.log("test error", error);
+  }
+
   return res.status(200).json({ message: "ok" });
 });
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,6 @@ import { loginUserController } from '../controllers/user.controllers.js';
 
 const router = Router();
 
-router.route('/login').get(loginUserController);
+router.route('/login').post(loginUserController);
 
 export default router;
