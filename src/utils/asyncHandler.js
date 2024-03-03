4ad2951a4c4022f3989e78d07b99d4d22d82ee9d
Mithun-18 <mithunpoojary180@gmail.com>
2024-03-03 22:57:26 +0530

    utility updated

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,8 +1,18 @@
-const asyncHandler = (requestHandler) => {
-    return (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+import { ApiError } from "./ApiError.js";
+
+const asyncHandler = (requestHandler, authorize = true) => {
+  return (req, res, next) => {
+    if (authorize) {
+      if (
+        !(typeof req.headers.userid == "string" && req.headers.userid != "")
+      ) {
+        res.status(401).json(new ApiError(401, "Unauthorized"));
+        return;
+      }
     }
-}
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
 
+export { asyncHandler };
 
-export { asyncHandler }
