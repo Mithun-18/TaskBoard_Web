d6b83e4b9bc1b1fad63c98ce53d21b83e916ce31
Mithun-18 <mithunpoojary180@gmail.com>
2024-03-03 15:33:18 +0530

    cors added

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin:process.env.FRONTEND_URL,
+  })
+);
 
 // Import Routes
 import userRoutes from "./routes/user.routes.js";
 
-app.use("/", (_, res) => {
+app.get("/", (_, res) => {
   res.send("Server is running...!");
 });
 
--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,5 +1,7 @@
-const loginUserController = (req, res) => {
-  res.send("hello");
-};
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const loginUserController = asyncHandler(async (req, res) => {
+  return res.status(200).json({ message: "ok" });
+});
 
 export { loginUserController };
